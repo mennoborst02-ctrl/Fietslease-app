adc372d92c384373db04984dc1d5a6f4a35efdd1
mennoborst02-ctrl <mennoborst02@gmail.com>
2026-05-13 15:15:19 +0200

    Initial commit

---
+++ db.js
@@ -0,0 +1,20 @@
+﻿const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+pool.query("SELECT 1").then(() => {
+  console.log("Database verbinding OK!");
+}).catch(err => {
+  console.error("Database verbinding FOUT:", err.message);
+});
+
+module.exports = pool;
---
+++ server.js
@@ -0,0 +1,25 @@
+﻿const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("de api werkt");
+});
+
+app.get("/bikes", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM bikes");
+    res.json(rows);
+  } catch (error) {
+    console.error("DATABASE FOUT:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server draait op poort 3001");
+});
